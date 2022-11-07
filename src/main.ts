8fc48dd36839d0b25ef9149cfac7df9715d9bca2
Luka Semolič <luka.semolic@intesosolutions.com>
2022-11-07 22:27:19 +0100

    creating a mockup

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,118 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import { setupCounter } from './counter'
 
+const small = document.getElementById('small')
+const large = document.getElementById('large')
+const circle = document.getElementById('circle')
+
+interface SliderVariable {
+  name: string;
+  value: number;
+  color: string;
+}
+
+class Slider {
+
+  width: number;
+  heigth: number
+  sliders: SliderVariable[];
+
+  constructor(width: number, height: number, sliders: SliderVariable[]) {  
+    this.width = width;
+    this.heigth = height;  
+    this.sliders = [...sliders];
+  }
+
+  draw = () => {
+    
+    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    svg.setAttribute('width', "" + this.heigth)
+    svg.setAttribute('height', "" + this.heigth)
+
+    let stepper = 60;
+    let stepperCircle = 36 * this.sliders.length;
+
+    this.sliders.reverse();
+
+    // we iterate sliders
+    this.sliders.map((s: SliderVariable, i: number) => {
+
+
+        const group = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+
+        const slider = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+        slider.setAttribute('cx', "" + this.width / 2);
+        slider.setAttribute('cy', "" + this.heigth / 2);
+        slider.setAttribute('r', stepper.toString());
+        slider.setAttribute('stroke-width', '18');
+        slider.setAttribute('stroke-dasharray', '7 1');
+        slider.setAttribute('stroke', s.color);
+        slider.setAttribute('fill', 'transparent');
+        group.appendChild(slider);
+
+        const sliderButton = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+        sliderButton.id = `slider-${i}`;
+        sliderButton.setAttribute('cx', "" + this.width / 2);
+        sliderButton.setAttribute('cy', "" + ((this.heigth / (this.sliders.length * 2)) + stepperCircle));
+        sliderButton.setAttribute('r', '13');
+        sliderButton.setAttribute('fill', 'white');
+        sliderButton.setAttribute('stroke-width', '1');
+        sliderButton.setAttribute('stroke', 'black');           
+        group.appendChild(sliderButton);       
+       
+        
+        svg.appendChild(group);
+
+
+      console.log('s', s)
+      stepper += 40
+      stepperCircle -= 40
+    })
+
+    document.querySelector<HTMLDivElement>('#app')!.appendChild(svg);
+  }
+}
+
+let slider1 = new Slider(
+  600,
+  600,
+  [{
+    name: 'Transportation',
+    value: 750,
+    color: 'purple',
+  }, {
+    name: 'Food',
+    value: 750,
+    color: 'blue',
+  }, {
+    name: 'Insurance',
+    value: 500,
+    color: 'green',
+  }, {
+    name: 'Entertainment',
+    value: 800,
+    color: 'orange',
+  }, {
+    name: 'Health Care',
+    value: 200,
+    color: 'red',
+  }]
+);
+slider1.draw();
+
+small && small.addEventListener('click', () => {
+  console.log('small-circle')
+})
+
+large && large.addEventListener('click', (e) => {
+  console.log('large-circle', e.clientX, e.clientY);
+})
+
+circle && circle.addEventListener('click', () => {
+  console.log('circle')  
+})
+
+/*
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
@@ -20,4 +132,4 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   </div>
 `
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)*/
