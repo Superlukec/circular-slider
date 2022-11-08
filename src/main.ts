0b1610c7bfa655708113826d7469678e708cf9a5
Luka Semolič <luka.semolic@intesosolutions.com>
2022-11-08 21:59:26 +0100

    Adding the layer to show the process; Remove unused code

--- src/main.ts
+++ src/main.ts
@@ -1,126 +1,128 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
-
-const small = document.getElementById('small')
-const large = document.getElementById('large')
-const circle = document.getElementById('circle')
+//import typescriptLogo from './typescript.svg'
+//import { setupCounter } from './counter'
 
 interface SliderVariable {
   name: string;
   value: number;
   color: string;
+  maxValue?: number;
+  minValue?: number;
+  step?: number;
 }
 
 class Slider {
 
+  container: string;
   width: number;
   heigth: number
   sliders: SliderVariable[];
 
-  constructor(width: number, height: number, sliders: SliderVariable[]) {  
+  constructor(
+    container: string,
+    // radius: number,
+    width: number,
+    height: number,
+    sliders: SliderVariable[]) {
+    this.container = container;
     this.width = width;
-    this.heigth = height;  
+    this.heigth = height;
     this.sliders = [...sliders];
   }
 
   generateArch = (radius: number) => {
 
-    /*
-      M centerX (centerY-outerRadius)
-      A outerRadius outerRadius 0 1 0 centerX (centerY+outerRadius)
-      A outerRadius outerRadius 0 1 0 centerX (centerY-outerRadius)
-      Z
-      M centerX (centerY-innerRadius)
-      A innerRadius innerRadius 0 1 1 centerX (centerY+innerRadius)
-      A innerRadius innerRadius 0 1 1 centerX (centerY-innerRadius)
-      Z
-    */
-
     let centerX = this.width / 2;
     let centerY = this.heigth / 2;
 
-    let outerRadius = radius;
-    let innerRadius = radius - 15;
-
     return `
-      M ${centerX} ${centerY - outerRadius}
-      A ${outerRadius} ${outerRadius} 0 1 0 ${centerX} ${centerY + outerRadius}
-      A ${outerRadius} ${outerRadius} 0 1 0 ${centerX} ${centerY - outerRadius}
-      Z
-      M ${centerX} ${centerY - innerRadius}
-      A ${innerRadius} ${innerRadius} 0 1 1 ${centerX} ${centerY + innerRadius}
-      A ${innerRadius} ${innerRadius} 0 1 1 ${centerX} ${centerY - innerRadius}
-      Z
+      M ${centerX}, ${centerY - radius}
+      A ${radius}, ${radius}, 0 1 0, ${centerX}, ${centerY + radius}    
     `
+  }
 
+  filterClick = (element: SVGElement, x: number, y: number): boolean => {
+
+    var dim = element.getBoundingClientRect();
+
+    //console.log(slider.getAttribute('cx'), slider.getAttribute('cy'), slider.getAttribute('r'));
+
+    return true;
   }
 
   draw = () => {
-    
+
     const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
     svg.setAttribute('width', "" + this.heigth)
     svg.setAttribute('height', "" + this.heigth)
 
-    let stepper = 60;
-    let stepperCircle = 36 * this.sliders.length;
-
-    this.sliders.reverse();
+    let stepper = this.sliders.length * 40 + 20;
+    let stepperCircle = 20;
 
     // we iterate sliders
     this.sliders.map((s: SliderVariable, i: number) => {
 
 
-        const group = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+      const group = document.createElementNS('http://www.w3.org/2000/svg', 'g');
 
-        
-          console.log(this.generateArch(stepper))
-
-          const slider = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-          slider.setAttribute('d', this.generateArch(stepper))
-          slider.setAttribute('fill', s.color);
-          slider.setAttribute('stroke-dasharray', '7 1');
-
-          slider.addEventListener('click', () => {
-            console.log('arch clicked')
-          })
-
-          /*
-          const slider = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-          slider.setAttribute('cx', "" + this.width / 2);
-          slider.setAttribute('cy', "" + this.heigth / 2);
-          slider.setAttribute('r', stepper.toString());
-          slider.setAttribute('stroke-width', '18');
-          slider.setAttribute('stroke-dasharray', '7 1');
-          slider.setAttribute('stroke', s.color);
-          slider.setAttribute('fill', 'transparent');*/
-          group.appendChild(slider);
-        
-
-        const sliderButton = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-        sliderButton.id = `slider-${i}`;
-        sliderButton.setAttribute('cx', "" + this.width / 2);
-        sliderButton.setAttribute('cy', "" + ((this.heigth / (this.sliders.length * 2)) + stepperCircle));
-        sliderButton.setAttribute('r', '13');
-        sliderButton.setAttribute('fill', 'white');
-        sliderButton.setAttribute('stroke-width', '1');
-        sliderButton.setAttribute('stroke', 'black');           
-        group.appendChild(sliderButton);       
-       
-        
-        svg.appendChild(group);
+      const slider: SVGElement = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+      slider.setAttribute('id', s.name)
+      slider.setAttribute('cx', "" + this.width / 2);
+      slider.setAttribute('cy', "" + this.heigth / 2);
+      slider.setAttribute('r', stepper.toString());
+      slider.setAttribute('stroke-width', '18');
+      slider.setAttribute('stroke-dasharray', '7 1');
+      slider.setAttribute('stroke', 'lightgray');
+      slider.setAttribute('fill', 'transparent');
 
 
-      console.log('s', s)
-      stepper += 40
-      stepperCircle -= 40
+      const progressSlider: SVGElement = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+      progressSlider.setAttribute('d', this.generateArch(stepper))
+      progressSlider.setAttribute('stroke-width', '18');
+      progressSlider.setAttribute('stroke-dasharray', '7 1');
+      progressSlider.setAttribute('stroke', s.color);
+      progressSlider.setAttribute('fill', 'transparent');
+      progressSlider.setAttribute('transform', `rotate(180, ${this.width / 2}, ${this.heigth / 2})`);
+
+
+      [slider, progressSlider].map((slidr) => slidr.addEventListener('click', (evt) => {
+        var e: any = evt.target;
+        var dim = e.getBoundingClientRect();
+        console.log('dim', dim.left, dim.right)
+        console.log(`${s.name} clicked`, 'X: ' + (evt.clientX - dim.left), 'Y: ' + (evt.clientY - dim.top))
+        /*
+        if (this.filterClick(slider, evt.clientX, evt.clientY)) {
+          console.log(`${s.name} clicked`, evt.clientX, evt.clientY)
+        }*/
+      }))
+      group.appendChild(slider);
+      group.appendChild(progressSlider);
+
+
+      const sliderButton = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+      sliderButton.id = `slider-${i}`;
+      sliderButton.setAttribute('cx', "" + this.width / 2);
+      sliderButton.setAttribute('cy', "" + ((this.heigth / (this.sliders.length * 2)) + stepperCircle));
+      sliderButton.setAttribute('r', '13');
+      sliderButton.setAttribute('fill', 'white');
+      sliderButton.setAttribute('stroke-width', '1');
+      sliderButton.setAttribute('stroke', 'gray');
+      group.appendChild(sliderButton);
+
+
+      svg.appendChild(group);
+
+
+      stepper -= 40
+      stepperCircle += 40
     })
 
-    document.querySelector<HTMLDivElement>('#app')!.appendChild(svg);
+    document.querySelector<HTMLDivElement>(this.container)!.appendChild(svg);
   }
 }
 
 let slider1 = new Slider(
+  '#app',
   600,
   600,
   [{
@@ -147,35 +149,3 @@ let slider1 = new Slider(
 );
 slider1.draw();
 
-small && small.addEventListener('click', () => {
-  console.log('small-circle')
-})
-
-large && large.addEventListener('click', (e) => {
-  console.log('large-circle', e.clientX, e.clientY);
-})
-
-circle && circle.addEventListener('click', () => {
-  console.log('circle')  
-})
-
-/*
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)*/
